8384b845dd12301a2df75e920fbff906a087c6be
Muhammad Saad Hasan <msaadhasanpk@gmail.com>
2024-02-09 12:34:23 +0500

    first commit just to see if it works

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "i210566"
+include(":app")
+ 
