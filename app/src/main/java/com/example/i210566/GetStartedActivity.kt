d85bb09ebcb85f1b5a004fb358cb58e59b1cc998
Muhammad Saad Hasan <msaadhasanpk@gmail.com>
2024-02-09 19:56:27 +0500

    GetStarted Page done
    Minor changes need to be made

---
+++ app/src/main/java/com/example/i210566/GetStartedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.i210566
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GetStartedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Set the content view to your layout
+        setContentView(R.layout.activity_get_started)
+    }
+}
--- app/src/main/java/com/example/i210566/MainActivity.kt
+++ app/src/main/java/com/example/i210566/MainActivity.kt
@@ -1,22 +1,49 @@
 package com.example.i210566
 
-import android.os.Build
+import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.example.i210566.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val window: Window = window
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.statusBarColor = ContextCompat.getColor(this, R.color.tealish)
+        val tvSignUpPrompt = findViewById<TextView>(R.id.tvSignUpPrompt)
+        val spannableString = SpannableString("Don’t have an account? Sign Up")
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                // Redirect to GetStartedActivity when "Sign Up" is clicked
+                val intent = Intent(this@MainActivity, GetStartedActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = true  // Set to true if you want underline
+                ds.color = ContextCompat.getColor(applicationContext, R.color.white)  // Use your color
+                ds.typeface = Typeface.create(ds.typeface, Typeface.BOLD)  // Make the text bold
+            }
+
+        }
+
+        val signUpStartIndex = spannableString.indexOf("Sign Up")
+        spannableString.setSpan(clickableSpan, signUpStartIndex, signUpStartIndex + "Sign Up".length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        tvSignUpPrompt.apply {
+            text = spannableString
+            movementMethod = LinkMovementMethod.getInstance()  // This makes the link clickable
+            highlightColor = getColor(android.R.color.transparent)  // Optional: change the highlight color on click
         }
     }
 }
