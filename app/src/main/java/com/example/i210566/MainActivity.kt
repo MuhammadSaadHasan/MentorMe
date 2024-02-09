91a9785e512b533c9941e446cfc8aa38d368ca46
Muhammad Saad Hasan <msaadhasanpk@gmail.com>
2024-02-09 16:47:23 +0500

    first commit just to see if it works

--- app/src/main/java/com/example/i210566/MainActivity.kt
+++ app/src/main/java/com/example/i210566/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.i210566
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.i210566.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(this, R.color.tealish)
+        }
     }
 }
