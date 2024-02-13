00bdd4ae7e070de9cb81c3b3c8b88a7877d06024
Muhammad Saad Hasan <msaadhasanpk@gmail.com>
2024-02-13 09:37:15 +0500

    drop a review page done

--- app/src/main/java/com/example/i210566/CooperProfile.kt
+++ app/src/main/java/com/example/i210566/CooperProfile.kt
@@ -1,11 +1,23 @@
 package com.example.i210566
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class CooperProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cooper_profile)
+
+        // Find the button by its ID
+        val dropReviewButton = findViewById<AppCompatButton>(R.id.dropreviewID)
+
+        // Set an OnClickListener to the button
+        dropReviewButton.setOnClickListener {
+            // Create an Intent to start the DropReview Activity
+            val intent = Intent(this, DropReview::class.java)
+            startActivity(intent)
+        }
     }
 }
