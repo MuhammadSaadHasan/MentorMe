d2603fbdb755ccbceb04751e248caa8864a46d9f
Muhammad Saad Hasan <msaadhasanpk@gmail.com>
2024-02-13 07:42:19 +0500

    Lets Find... Page done

--- app/src/main/java/com/example/i210566/HomePageActivity.kt
+++ app/src/main/java/com/example/i210566/HomePageActivity.kt
@@ -1,11 +1,29 @@
 package com.example.i210566
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.i210566.R
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+
+
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_search -> {
+                    val intent = Intent(this, LetsFindActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
