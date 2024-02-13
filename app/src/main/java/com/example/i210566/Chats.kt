187016b91665278401a76d430ba6bf7e5b68e04a
Muhammad Saad Hasan <msaadhasanpk@gmail.com>
2024-02-13 15:55:05 +0500

    John Cooper DM Done

--- app/src/main/java/com/example/i210566/Chats.kt
+++ app/src/main/java/com/example/i210566/Chats.kt
@@ -1,11 +1,19 @@
 package com.example.i210566
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Chats : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chats)
+    }
+
+    // Method to handle RelativeLayout click
+    fun redirectToJohnCoopersDMs(view: View) {
+        val intent = Intent(this, JohnCoopersDMs::class.java)
+        startActivity(intent)
     }
 }
