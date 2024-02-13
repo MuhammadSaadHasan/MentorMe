3bfe67ce17ae64bdcdffcdbc40436a6bcb03aad2
Muhammad Saad Hasan <msaadhasanpk@gmail.com>
2024-02-13 09:00:01 +0500

    John cooper Profile Done

---
+++ app/src/main/java/com/example/i210566/CooperProfile.kt
@@ -0,0 +1,11 @@
+package com.example.i210566
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CooperProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cooper_profile)
+    }
+}
--- app/src/main/java/com/example/i210566/HomePageActivity.kt
+++ app/src/main/java/com/example/i210566/HomePageActivity.kt
@@ -3,16 +3,13 @@ package com.example.i210566
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.example.i210566.R
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
-
-
-
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigationView.setOnItemSelectedListener { item ->
@@ -24,6 +21,13 @@ class HomePageActivity : AppCompatActivity() {
                 }
                 else -> false
             }
+        }
+
+        // Add OnClickListener to the John Cooper FrameLayout
+        val frameLayoutJohnCooper = findViewById<FrameLayout>(R.id.frameLayoutJohnCooper)
+        frameLayoutJohnCooper.setOnClickListener {
+            val intent = Intent(this, CooperProfile::class.java)
+            startActivity(intent)
         }
     }
 }
